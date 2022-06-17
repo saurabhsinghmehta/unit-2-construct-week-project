57831f046f32f6043a91e9a56279b79cbbc2f571
Saurabh Singh <95415082+saurabhsinghmehta@users.noreply.github.com>
2022-06-17 16:19:55 +0530

    new files added

---
+++ home.js
@@ -0,0 +1,15 @@
+document.querySelector("#search").addEventListener("click", widthBottom);
+function widthBottom(){
+    var search=document.querySelector("#search");
+    search.style.borderBottom="4px solid black"
+    // console.log("hihoih")
+   
+}
+document.querySelector("div").addEventListener("click", widthBottomRemove);
+function widthBottomRemove(){
+    var search=document.querySelector("#search");
+    search.style.borderBottom="2px solid black"
+    // console.log("hihoih")
+   
+}
+// button functionality 
